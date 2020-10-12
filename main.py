2335fc41f14c801421419a21ac784cb193f919f1
sobbinLee <sooblair84@com>
2020-10-12 17:09:06 +0900

    #1.2 Getting Total cases by country

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+daily_df = pd.read_csv("data/daily_reports.csv")
+totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
+totals_df = totals_df.rename(columns={"index":"condition"})
