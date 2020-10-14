32a7427d77671d125df4c205cfa8fb90a6983568
sobbinLee <sooblair84@com>
2020-10-14 17:12:10 +0900

    #1.3 Global Time Series Part One

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ import pandas as pd
 daily_df = pd.read_csv("data/daily_reports.csv")
 totals_df = daily_df[["Confirmed", "Deaths", "Recovered"]].sum().reset_index(name="count")
 totals_df = totals_df.rename(columns={"index":"condition"})
+
+countries_df = daily_df[["Country_Region","Confirmed","Deaths","Recovered"]]
+countries_df = countries_df.groupby("Country_Region").sum().reset_index()
