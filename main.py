f2969c2f3b184e3076f44d0694be9c7b0f4d1bca
sobbinLee <sooblair84@com>
2020-10-22 14:12:53 +0900

    edit Style and edit css of barGraph

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-from data import countries_df
+from data import countries_df, totals_df
 from builders import make_table
 
 
@@ -23,8 +23,10 @@ bubble_map = px.scatter_geo(countries_df,
 size="Confirmed",
 hover_name="Country_Region",
 size_max=40,
+title="Confirmed By Country",
 template="plotly_dark",
-projection="natural earth",
+color_continuous_scale = px.colors.sequential.Oryel,
+
 hover_data={
 "Confirmed":":,2f",
 "Deaths":":,2f",
@@ -33,6 +35,33 @@ hover_data={
 }, color="Confirmed", 
     locations="Country_Region",
     locationmode="country names")
+
+#bubble_map changing layout
+bubble_map.update_layout(margin=dict(l=0,r=0,t=50,b=0)) 
+
+# barGraph
+bars_graph = px.bar(totals_df, x="condition", y="count",
+hover_data={
+    'count':":,"
+},
+ template="plotly_dark", 
+ title="Total Global Cases",
+ labels={
+     "conditon":"Condition",
+     "count":"Count",
+     "color": "Condition"
+    }
+ )
+
+#bar의 제목 설정 x좌표, y좌표 각각 설정
+#bars_graph.update_layout(
+#    xaxis=dict(title="Condition"),
+#    yaxis=dict(title="Count")
+#) -> labels 설정을 통해 똑같은 세팅을 할 수 있다.
+ 
+#bar color 지정
+bars_graph.update_traces(marker_color=["#ff4d4d","#7d5fff","#32ff7e"])
+
 
 app.layout = html.Div(
     style={
@@ -48,27 +77,29 @@ app.layout = html.Div(
         ),
         html.Div(
             style={
-                "display": "grid",
-                "gap": 50,
-                "gridTemplateColumns": "repeat(4, 1fr)",
+                "display":"grid",
+                "gap":50,
+                "gridTemplateColumns":"repeat(4, 1fr)"
             },
             children=[
                 html.Div(
-                    style={"grid-column": "span 3"},
-                    children=[dcc.Graph(figure=bubble_map)],
-                ),
+                    style={"grid-column":"span 3"},
+                    children=[dcc.Graph(figure=bubble_map)]),
                 html.Div(children=[make_table(countries_df)]),
             ],
-        ),
-        html.Div(
-            style={
-                "display": "grid",
-                "gap": 50,
-                "gridTemplateColumns": "repeat(4, 1fr)",
-            },
+        ), html.Div(
             children=[
-                    ],
-                ),
+                html.Div(
+                    style={
+                        "display":"grid",
+                        "gap":50,
+                        "gridTemplateColumns":"repeat(4, 1fr)"
+                    },
+                    children=[dcc.Graph(figure=bars_graph)]),
+      
+            ],
+        ),
+ 
             ],
         )
 
